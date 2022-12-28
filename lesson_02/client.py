46fa4f87146cbb2061dd10a1f7fc0c1df7333dbb
Gmurad735 <abd.gm90@yandex.ru>
2022-12-28 11:02:13 +0300

    add hw

--- lesson_02/client.py
+++ lesson_02/client.py
@@ -1,29 +1,35 @@
 # Import socket module
 import socket            
 import pickle
+
 # Create a socket object
 s = socket.socket()        
  
 # Define the port on which you want to connect
-port = 8002               
+port = 8005               
  
 # connect to the server on local computer
 s.connect(('127.0.0.1', port))
+
 # The client send this line to server
 urls_file_path = "./data/urls.txt"
 s.send(urls_file_path.encode())
 # receive data from the server and decoding to get the string.
 data = []
+packet = 1
 while True:
-    packet = s.recv(4096)
+    packet = s.recv(1024)  
     if not packet: break
+    message = packet
     data.append(packet)
-data_arr = pickle.loads(b"".join(data))
-print(data_arr)
-with open("client_data.txt", "a", encoding="utf-8") as output_file:
-    output_file.write(str(data_arr))
+    data_arr = pickle.loads(b"".join(data))
+    
+    with open("client_data.txt", "w", encoding="utf-8") as output_file:
+         for item in data_arr:
+                output_file.write(str(item[0]) +": " + str(item[1]) + "\n")
 
-
-print(s.recv(1024).decode())
+    #print(message.decode())
+#print(s.recv(1024).decode())
+#print(s.recv(1024).decode())
 # close the connection
 s.close()
--- lesson_02/server.py
+++ lesson_02/server.py
@@ -7,51 +7,57 @@ import re
 import pickle
 
 def get_n_often_words(url, n = 10):
+    """Get text from url and return n offten words in text"""
+    
+    # get html document
     html = urlopen(url).read()
     soup = BeautifulSoup(html, features = "html.parser")
     # kill all script and style elements
     for script in soup(["script", "style"]):
         script.extract()    # rip it out
-    
+    # get text from body
     text = soup.body.get_text()
     # break into lines and remove leading and trailing space on each
     lines = (line.strip() for line in text.splitlines())
     # break multi-headlines into a line each
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     text = '\n'.join(chunk for chunk in chunks if chunk)
+    
+    # words counter
     counter = Counter()
+    # remove puctuations
     all_words = re.findall(r"\w+", text.lower())
     words = []
+    # remove prepositions
     for word in all_words:
         if len(word) > 1:
             words.append(word)
     counter.update(words)
     most_common = counter.most_common(n)
-    result = []
-    for item in most_common:
-        result.append(item[0])
-    return result
+    
+    return most_common
     
     
-# next create a socket object
+# create a socket object
 s = socket.socket()        
 print ("Socket successfully created")
  
 # reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 8002               
+# case it is 8005 but it can be anything
+port = 8005               
  
 # Next bind to the port
 # we have not typed any ip in the ip field
 # instead we have inputted an empty string
 # this makes the server listen to requests
 # coming from other computers on the network
+
 s.bind(('', port))        
 print ("socket binded to %s" %(port))
  
 # put the socket into listening mode
 s.listen(5)    
-print ("socket is listening")           
+print ("socket is listening...")           
  
 # a forever loop until we interrupt it or
 # an error occurs
@@ -59,7 +65,7 @@ while True:
     # Establish connection with client.
     c, addr = s.accept()    
     print ('Got connection from', addr )
-    recived_path = c.recv(1024).decode()
+    recived_path = c.recv(4096).decode()
     with open(recived_path) as urls_file:
         for url in urls_file:       
             top_10 = get_n_often_words(url.strip())
@@ -67,8 +73,8 @@ while True:
             c.send(data)
     
     # send a thank you message to the client. encoding to send byte type.
-    c.send('Thank you for connecting'.encode())
-    
+    #c.send('Process finished'.encode())
+   # c.send('Thank you for connecting'.encode())
     
     # Close the connection with the client
     c.close()
